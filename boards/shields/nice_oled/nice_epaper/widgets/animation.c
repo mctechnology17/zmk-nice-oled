64179eb868faea8fcf63e2ce9d307c77cdbea305
J-Jaywalker <jamesjarodwalker@hotmail.co.uk>
2025-07-17 15:57:07 +0100

    Test to add lines to peripheral

--- boards/shields/nice_oled/nice_epaper/widgets/animation.c
+++ boards/shields/nice_oled/nice_epaper/widgets/animation.c
@@ -3,26 +3,9 @@
 #include "animation.h"
 
 LV_IMG_DECLARE(crystal_01);
-LV_IMG_DECLARE(crystal_02);
-LV_IMG_DECLARE(crystal_03);
-LV_IMG_DECLARE(crystal_04);
-LV_IMG_DECLARE(crystal_05);
-LV_IMG_DECLARE(crystal_06);
-LV_IMG_DECLARE(crystal_07);
-LV_IMG_DECLARE(crystal_08);
-LV_IMG_DECLARE(crystal_09);
-LV_IMG_DECLARE(crystal_10);
-LV_IMG_DECLARE(crystal_11);
-LV_IMG_DECLARE(crystal_12);
-LV_IMG_DECLARE(crystal_13);
-LV_IMG_DECLARE(crystal_14);
-LV_IMG_DECLARE(crystal_15);
-LV_IMG_DECLARE(crystal_16);
 
 const lv_img_dsc_t *anim_imgs[] = {
-    &crystal_01, &crystal_02, &crystal_03, &crystal_04, &crystal_05, &crystal_06,
-    &crystal_07, &crystal_08, &crystal_09, &crystal_10, &crystal_11, &crystal_12,
-    &crystal_13, &crystal_14, &crystal_15, &crystal_16,
+    &crystal_01,
 };
 
 void draw_animation(lv_obj_t *canvas, struct zmk_widget_screen *widget) {
